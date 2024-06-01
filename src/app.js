24393838859b05128f5b1d5a124e5384e141b9a3
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2024-06-01 12:25:40 -0600

    Chat finalizado

--- src/app.js
+++ src/app.js
@@ -15,10 +15,18 @@ app.set('view engine','handlebars');
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 
-app.use('/',viewsRouter);
+
 
 const server = app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
 const socketServer = new Server(server);
+
+//Propuesta para conectar socket.io con un request habitual GET POST PUT DELETE etc
+// app.use((req,res,next)=>{
+//     req.io = socketServer
+//     next();
+// })
+
+app.use('/',viewsRouter);
 
 const messages = [];
 
@@ -26,7 +34,18 @@ socketServer.on('connection',(socketClient)=>{
     //callback a ejecutar.
     console.log("Cliente conectado con id: ",socketClient.id);
 
+    //Si llegó a este punto, el usuario ahora está listo para chatear.
+    socketServer.emit('log',messages);
+
+    socketClient.on('authenticated',data=>{
+        //El server lo envía a TODOS
+        //El socketClient, lo envía únicamente al cliente.
+        //El socket BROADCAST lo envía a TODOS, menos al cliente.
+        socketClient.broadcast.emit('newUserConnected',data)
+    })
+
     socketClient.on("message",data=>{
+        console.log(data);
         messages.push(data);
         //Ojito con el emit
         socketServer.emit('log',messages)
--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,22 +1,57 @@
-const socket = io();
-
+const socket = io({
+  autoConnect: false,
+});
 const chatbox = document.getElementById('chatbox');
+let username;
 
-chatbox.addEventListener('keyup',(event)=>{
-    if(event.key==="Enter"){
-        //Aquí significaría enviar el mensaje
-        socket.emit("message",chatbox.value);
-        chatbox.value="";
+Swal.fire({
+  title: 'Identifícate',
+  icon: 'question',
+  input: 'text',
+  inputValidator: (value) => {
+    if (!value) {
+      return '¡Necesitas escribir un nombre de usuario para participar en el chat!';
     }
-})
+  },
+  allowOutsideClick: false,
+  allowEscapeKey: false,
+}).then((response) => {
+  username = response.value;
+  socket.connect();
+  socket.emit('authenticated',username);
+});
 
+chatbox.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    if (chatbox.value.trim()) {
+      //Aquí significaría enviar el mensaje
+      socket.emit('message', { username: username, message: chatbox.value.trim() });
+      chatbox.value = '';
+    }else{
+        //Aquí ponte creativo, puedes ignorarlo o decirle que no puede enviar cosas vacías
+    }
+  }
+});
 
 //Events
-socket.on('log',data=>{
-    const logs = document.getElementById('messagesLog');
-    let messages = "";
-    data.forEach(message=>{
-        messages+=`${socket.id} dice: ${message} <br/>`
+socket.on('log', (data) => {
+  const logs = document.getElementById('messagesLog');
+  let messages = '';
+  data.forEach((logItem) => {
+    messages += `${logItem.username} dice: ${logItem.message} <br/>`;
+  });
+  logs.innerHTML = messages;
+});
+
+
+socket.on('newUserConnected',(data)=>{
+    if(!username) return;
+    Swal.fire({
+        toast:true,
+        showConfirmButton:false,
+        timer:3000,
+        title:`${data} se ha unido al chat`,
+        position:'top-end',
+        icon:"success"
     })
-    logs.innerHTML = messages;
 })
